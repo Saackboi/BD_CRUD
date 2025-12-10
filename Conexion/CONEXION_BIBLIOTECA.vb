5c37e19f975c7188964792a8754049e85824dff5
Kevin2523 <kjmg2325@gmail.com>
2025-12-10 12:34:29 -0100

    Diseño

--- Conexion/CONEXION_BIBLIOTECA.vb
+++ Conexion/CONEXION_BIBLIOTECA.vb
@@ -4,16 +4,13 @@ Imports System.Configuration
 
 Module CONEXION_BIBLIOTECA
 
-    ' ACA DEBES REEMPLAZAR (localdb)\MSSQLLocalDB y la clave por tu configuración real de SQL Server '
-    Public CONEXION As New SqlConnection(
-    "Server=localhost\SQLEXPRESS;Database=DB_CRUD_VB_SQLSERVER_BIBLIOTECJA_ASPNET;User Id=sa;Password=@Jhair5223;"
-       )
+    Public CONEXION As New SqlConnection("Server=localhost; Database=DB_CRUD_VB_SQLSERVER_BIBLIOTECJA_ASPNET;User Id=kevin;Password=12345")
 
     Public Sub CONECTAR_BIBLIOTECA()
         Try
-            ' se verifica si la conexion esta cerrada’
+            ' Se verifica si la conexion esta cerrada
             If CONEXION.State = ConnectionState.Closed Then
-                CONEXION.Open() ' abrimos la conexion ‘ 
+                CONEXION.Open() ' Abrimos la conexion
             End If
         Catch ex As Exception
             MsgBox("ERROR DE CONEXION A LA BASE DE DATOS" & vbCrLf & vbCrLf & ex.Message, MsgBoxStyle.Critical, "ERROR DE CONEXION")
@@ -22,14 +19,13 @@ Module CONEXION_BIBLIOTECA
 
     Public Sub DESCONECTAR_BIBLIOTECA()
         Try
-            ' cerrar la conexion solo si esta abierta, pa evitar errores 
+            ' Cerrar la conexion solo si esta abierta, pa evitar errores
             If CONEXION.State = ConnectionState.Open Then
-                CONEXION.Close() ' cerramos para liberar recursos, IMPORTANTE NUNCA dejarla abierta
+                CONEXION.Close() ' Cerramos para liberar recursos, IMPORTANTE NUNCA dejarla abierta
             End If
         Catch ex As Exception
             MsgBox("ERROR AL DESCONECTAR DE LA BASE DE DATOS" & vbCrLf & vbCrLf & ex.Message, MsgBoxStyle.Critical, "ERROR DE CONEXION")
         End Try
     End Sub
-
 
 End Module
